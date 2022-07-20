86503e40cce373411dd3138ea451c95807141714
wsj321 <wsj321@hanmail.net>
2022-07-20 14:16:18 +0900

    정규표현식

---
+++ DemoRE.py
@@ -0,0 +1,19 @@
+# DemoRE.py
+
+import re
+#함정이다!
+result = re.search("[0-9]*th", "35th")
+print(result)
+print(result.group())
+
+# result = re.match("[0-9]*th", "35th")
+# print(result)
+# print(result.group())
+
+print(bool(re.search("apple", "this is apple")))
+print(bool(re.match("apple", "this is apple")))
+
+result = re.search("\d{4}", "올해는 2022년입니다. ")
+print(result.group())
+result = re.search("\d{5}", "우리동네는 52100입니다. ")
+print(result.group())
---
+++ DemoStr.py
@@ -0,0 +1,36 @@
+# DemoStr.py
+
+# print(dir(str))
+
+strA = "파이썬은 강력해"
+strB = "python is very powerful"
+print(len(strA))
+print(len(strB))
+print(strB.capitalize())
+print(strB.startswith("py"))
+print(strB.endswith("ful"))
+print(strB.endswith("ful", 7))
+print(strB.count("p"))
+print(strB.count("p", 7))
+
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+print("2580".isdecimal())
+
+# u = " spam and ham"
+# result = u.strip("<>")
+# print(u)
+# print(result)
+
+u = " <<< spam and ham >>> "
+# 앞뒤에 있는 어떤 문자
+result = u.strip("<>")
+print(u)
+print(result)
+
+result = result.replace("spam", "spam egg")
+print(result)
+lst = result.split()
+print(lst)
+print(":)".join(lst))
+
