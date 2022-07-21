651e9d0cd61c36efd55170303fd15f6a0e93de71
wsj321 <wsj321@hanmail.net>
2022-07-21 14:46:40 +0900

    두번째 GUI화면

--- DemoForm.py
+++ DemoForm.py
@@ -16,8 +16,8 @@ class DemoForm(QDialog, form_class):
         self.label.setText("첫번째 Qt화면")
 
 #진입점을 체크
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    demoWindow = DemoForm()
-    demoWindow.show() 
-    app.exec_()  
+#if __name__ == "__main__":
+app = QApplication(sys.argv)
+demoWindow = DemoForm()
+demoWindow.show() 
+app.exec_()  
