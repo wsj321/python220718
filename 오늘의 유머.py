149bfa0d1e8e5fd4eb888ad7109fa541b574ba87
wsj321 <wsj321@hanmail.net>
2022-07-21 13:40:53 +0900

    클리앙중고장터 검색

--- 오늘의 유머.py
+++ 오늘의 유머.py
@@ -7,9 +7,9 @@ import re
 #User-Agent를 조작하는 경우(아이폰에서 사용하는 사파리 브라우져의 헤더) 
 hdr = {'User-agent':'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/603.1.23 (KHTML, like Gecko) Version/10.0 Mobile/14E5239e Safari/602.1'}
 
-for n in range(0,10):
-        #클리앙의 중고장터 주소 
-        data ='https://www.clien.net/service/board/sold?&od=T31&po=' + str(n)
+for n in range(1,11):
+        #오늘의 유머 주소  
+        data ='http://www.todayhumor.co.kr/board/list.php?table=bestofbest&page=' + str(n)
         print(data)
         #웹브라우져 헤더 추가 
         req = urllib.request.Request(data, \
@@ -18,17 +18,17 @@ for n in range(0,10):
         #한글이 깨지는 경우 디코딩
         page = data.decode('utf-8', 'ignore')
         soup = BeautifulSoup(page, 'html.parser')
-        # <span class="subject_fixed" data-role="list-title-text">
-        #     아이패드 프로 3세대 12.9 인치 256g Wifi 버전 (스마트 키보드 폴리오 케이스 포함)
-        # </span>
+
+        # <td class="subject">
+        #     <a href="/board/view.php?table=bestofbest&no=457514&s_no=457514&page=1" target="_top">인도 여행가지 말라는 이유</a>
         #속성들(attrs)
-        list = soup.find_all('span', attrs={'data-role':'list-title-text'})
+        list = soup.find_all('td', attrs={'class':'subject'})
 
         for item in list:
                 try:
-                        title = item.text.strip() 
+                        title = item.find("a").text.strip()  
                         #print(title)
-                        if (re.search('아이패드', title)):
+                        if (re.search('미국', title)):
                                 print(title)
                 except:
                         pass
